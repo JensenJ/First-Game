50b3a94d875bc002b3813215783a1c247e443aae
JensenJ <jensenbradley@icloud.com>
2017-09-09 18:20:15 +0100

    Physics added to Player
    Player now rolls forward

---
+++ First-Game/Assets/PlayerMovement.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log("Started Player Initialization");
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        rb.AddForce(0, 0, 2000 * Time.deltaTime); //add a force of 2000 on z axis
+	}
+}
