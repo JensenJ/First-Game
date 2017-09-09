216dcb7d44fc907e30fd50ee19e9ebdfb5be7f90
JensenJ <jensenbradley@icloud.com>
2017-09-09 19:51:31 +0100

    Added Collision and Simple Map

--- First-Game/Assets/PlayerMovement.cs
+++ First-Game/Assets/PlayerMovement.cs
@@ -16,11 +16,11 @@ public class PlayerMovement : MonoBehaviour {
 	void FixedUpdate () {
         rb.AddForce(0, 0, forwardForce * Time.deltaTime); //add a force of 2000 on z axis
 
-        if (Input.GetKey("d"))
+        if (Input.GetKey("d")) // Check for input when pressing "d"
         {
             rb.AddForce(directionalForce * Time.deltaTime, 0, 0);
         }
-        if (Input.GetKey("a"))
+        if (Input.GetKey("a")) // Check for input when pressing "a"
         {
             rb.AddForce(-directionalForce * Time.deltaTime, 0, 0);
         }
