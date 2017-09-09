00d7af53169d0c789884fc40217564ba57c80279
JensenJ <jensenbradley@icloud.com>
2017-09-09 18:53:15 +0100

    Player Movement
    Player movement has been implemented and also the level surface has no friction anymore, meaning the player just glides along it instead of rolling.

--- First-Game/Assets/PlayerMovement.cs
+++ First-Game/Assets/PlayerMovement.cs
@@ -4,6 +4,9 @@ public class PlayerMovement : MonoBehaviour {
 
     public Rigidbody rb;
 
+    public float forwardForce = 2000f;
+    public float directionalForce = 500f;
+
 	// Use this for initialization
 	void Start () {
         Debug.Log("Started Player Initialization");
@@ -11,6 +14,18 @@ public class PlayerMovement : MonoBehaviour {
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-        rb.AddForce(0, 0, 2000 * Time.deltaTime); //add a force of 2000 on z axis
-	}
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime); //add a force of 2000 on z axis
+
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(directionalForce * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-directionalForce * Time.deltaTime, 0, 0);
+        }
+
+
+
+    }
 }
