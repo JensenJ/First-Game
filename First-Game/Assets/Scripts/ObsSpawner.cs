67d37624a6bb629e410b70e84480fa8d3a86d557
JensenJ <jensenbradley@icloud.com>
2017-09-09 23:02:48 +0100

    Fixed Global Namespace Error

--- First-Game/Assets/Scripts/ObsSpawner.cs
+++ First-Game/Assets/Scripts/ObsSpawner.cs
@@ -2,13 +2,28 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ObstacleSpawner : MonoBehaviour {
+public class ObsSpawner : MonoBehaviour
+{
 
     public Transform[] spawnPoints;
 
     public GameObject blockPrefab;
-	// Use this for initialization
-	void Start () {
+
+    private float timeToSpawn = 1f;
+
+    public float spawnRate = 10f;
+
+    //void Update()
+    //{
+    //    if (Time.time >= timeToSpawn)
+    //    {
+    //        SpawnBlocks();
+    //        timeToSpawn = Time.time + spawnRate;
+    //    }
+    //}
+
+    void Start()
+    {
         int randomIndex = Random.Range(0, spawnPoints.Length);
 
         for (int i = 0; i < spawnPoints.Length; i++)
@@ -18,10 +33,5 @@ public class ObstacleSpawner : MonoBehaviour {
                 Instantiate(blockPrefab, spawnPoints[i].position, Quaternion.identity);
             }
         }
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    }
 }
