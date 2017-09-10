378636f50b76fcf1092c2de10fa2597b118eb6cf
JensenJ <jensenbradley@icloud.com>
2017-09-10 18:36:41 +0100

    destruction of objects after player has passed

--- First-Game/Assets/Scripts/ObsSpawner.cs
+++ First-Game/Assets/Scripts/ObsSpawner.cs
@@ -6,16 +6,12 @@ public class ObsSpawner : MonoBehaviour
 {
 
     public Transform[] spawnPoints; //makes this option available in unity
+    public Transform floorSpawnPoint; //makes this option available in unity
 
     public GameObject blockPrefab; //makes this option available in unity
-
-    
-
-    void Update() //updates every tick
-    { 
-        
-          
-    }
+    public GameObject floorPrefab; //makes this option available in unity
+    public GameObject floorInstance;
+    public GameObject blockInstance;
 
     public void SpawnBlocks()
     {
@@ -25,13 +21,18 @@ public class ObsSpawner : MonoBehaviour
         {
             if (randomIndex != i)
             {
-                Instantiate(blockPrefab, spawnPoints[i].position, Quaternion.identity);// spawns blocks accross the row apart from the one block randomly selected
+                blockInstance = (GameObject)Instantiate(blockPrefab, spawnPoints[i].position, Quaternion.identity);// spawns blocks accross the row apart from the one block randomly selected
+                Destroy(blockInstance, 5);
             }
         }
     }
-    void OnTriggerEnter()
+    void OnTriggerEnter()// when the player enters the trigger box
     {
-        SpawnBlocks();
-        transform.Translate(0, 0, 45);
+        SpawnBlocks(); //calls spawn blocks method
+        transform.Translate(0, 0, 45); //moves spawners and trigger forward
+        floorInstance = (GameObject)Instantiate(floorPrefab, floorSpawnPoint.position, Quaternion.identity); // spawns the floor in for the next wave
+        Destroy(floorInstance, 5);
+        
     }
 }
+
