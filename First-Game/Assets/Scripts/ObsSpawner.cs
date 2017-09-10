da33f863c27e3a9f250e6876ca1c17be9aef97f0
JensenJ <jensenbradley@icloud.com>
2017-09-10 15:50:51 +0100

    Procedural Spawning optimised
    Made blocks only spawn as needed by adding trigger box infront of them

--- First-Game/Assets/Scripts/ObsSpawner.cs
+++ First-Game/Assets/Scripts/ObsSpawner.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -10,23 +9,17 @@ public class ObsSpawner : MonoBehaviour
 
     public GameObject blockPrefab; //makes this option available in unity
 
-    private float timeToSpawn = 1f;
-
-    private float spawnRate = 1f;
+    
 
     void Update() //updates every tick
-    {
-        if (Time.time >= timeToSpawn)
-        {
-            SpawnBlocks();
-            transform.Translate(0, 0, 160); //Moves obstacle spawners forward after blocks have been spawned
-            timeToSpawn = Time.time + spawnRate; //Resets cycle
-        }
+    { 
+        
+          
     }
 
-    void SpawnBlocks()
+    public void SpawnBlocks()
     {
-        int randomIndex = UnityEngine.Random.Range(0, spawnPoints.Length); //Works out where to spawn the blocks
+        int randomIndex = Random.Range(0, spawnPoints.Length); //Works out where to spawn the blocks
 
         for (int i = 0; i < spawnPoints.Length; i++)
         {
@@ -35,5 +28,10 @@ public class ObsSpawner : MonoBehaviour
                 Instantiate(blockPrefab, spawnPoints[i].position, Quaternion.identity);// spawns blocks accross the row apart from the one block randomly selected
             }
         }
+    }
+    void OnTriggerEnter()
+    {
+        SpawnBlocks();
+        transform.Translate(0, 0, 45);
     }
 }
